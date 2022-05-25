72f6a89d0c94a0eca030e5f8cbf80af0791e8a46
Gonzalo Gómez <gonzagom66@yahoo.com.ar>
2022-05-25 13:49:23 -0300

    useState hook

--- counter-app/src/CounterApp.js
+++ counter-app/src/CounterApp.js
@@ -1,15 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
 const CounterApp = ({ value }) => {
-    const clickEventHandler = (e) => {
-        console.log(e)
+
+    const [counter, setCounter] = useState(0)
+
+    const clickEventHandler = () => {
+        setCounter(counter+1)
+        // setCounter((c) => c+1) Si no tengo acceso a la variable de esta forma puedo aumentar el contador
     }
 
     return (
     <>
         <h1>CounterApp</h1>
-        <h2>{ value }</h2>
+        <h2>{ counter }</h2>
 
         <button onClick={ clickEventHandler }>+1</button>
     </>
