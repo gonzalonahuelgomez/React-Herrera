422ca4bed6f1d368358dc8b53055e3b8b9a05a45
Gonzalo Gómez <gonzagom66@yahoo.com.ar>
2022-06-02 12:48:24 -0300

    use effect gifexpertapp

--- gif-expert-app/src/GifExpertApp.js
+++ gif-expert-app/src/GifExpertApp.js
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
+import AddCategory from './components/AddCategory'
+import GifGrid from './components/GifGrid'
 
 const GifExpertApp = () => {
+
+  const animes = ['Saint Seiya'/*, 'Bleach', 'Kimetsu no Yaiba'*/]
+  const [categories, setCategories] = useState(animes)
+
+  // const handleAdd = () => {
+  //   let newAnime = 'HunterXHunter'
+  //   setCategories(categories.concat(newAnime))
+  //   setCategories([...categories, newAnime])
+  // }
+
   return (
       <>
         <h2>GifExpertApp</h2>
+        <AddCategory setCategories={setCategories}/>
         <hr/>
+
+        {/* <button onClick={handleAdd}>Agregar</button> */}
+
+        <ol>
+          { 
+            categories.map(category =>
+              <GifGrid
+                key={ category }
+                category={ category }/>
+            )
+          }
+        </ol>
       </>
   )
 }
---
+++ gif-expert-app/src/components/GifGrid.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react'
+import PropTypes from 'prop-types'
+import GifGridItem from './GifGridItem'
+
+const GifGrid = ({category}) => {
+
+    const [images, setImages] = useState([])
+
+    useEffect( () => {
+        getGifs()
+    }, [])
+    
+    const getGifs = async() => {
+        const url = 'https://api.giphy.com/v1/gifs/search?q=Rick+and+Morty&limit=10&api_key=682MJ9o1nM0YXrVg5MP7HKwC1x7mFc7V'
+        const resp = await fetch(url)
+        const { data } = await resp.json()
+
+        const gifs = data.map(({ id,title,images }) => {
+            return({
+                id,
+                title,
+                images: images?.downsized_medium.url
+            })
+        })
+
+        setImages(gifs)        
+    }
+
+    return (
+        <div>
+            <h3>{category}</h3>
+            {
+                images.map(img => 
+                    <GifGridItem
+                        key={img.id}
+                        {...img}/>
+                )
+            }          
+        </div>
+    )
+}
+
+GifGrid.propTypes = {}
+
+export default GifGrid
