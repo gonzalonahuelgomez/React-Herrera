243a810fc34ae0b76aa088f75dfe503b1f7e1568
Gonzalo Gómez <gonzalo.gomez@improntait.com>
2022-09-21 19:12:35 -0300

    create hooks

--- hook-app/src/01-useState/CounterApp.jsx
+++ hook-app/src/01-useState/CounterApp.jsx
@@ -1,22 +1,36 @@
 import React from 'react'
 import { useState } from 'react'
 
-const CounterApp = ({initialState}) => {
-    const [{ counter1, counter2, counter3 }, setCounter] = useState({counter1: 10, counter2: 20, counter3: 30})
+const CounterApp = ({ initialState }) => {
+    const [counter, setCounter] = useState({ counter1: 10, counter2: 20, counter3: 30 })
+    const { counter1, counter2, counter3 } = counter
 
-    const addToCounter = () => setCounter({counter1: counter1 + 1, counter2: counter2, counter3: counter3})
-    const substractToCounter = () => setCounter(counter1 - 1)
-    const resetCounter = () => setCounter(initialState)
+    const addToCounter = () => 
+        setCounter({ 
+            counter1: counter1 + 1, 
+            counter2: counter2 + 1, 
+            counter3: counter3 + 1 
+        })
+
+    const substractToCounter = () => 
+        setCounter({ 
+            ...counter, 
+            counter1: counter1 - 1 
+        })
+
+    const resetCounter = () => 
+        setCounter(counter)
 
 
     return (
         <>
-            <div>{counter1}</div>
-            <div>{counter2}</div>
-            <div>{counter3}</div>
-            <button onClick={addToCounter}>+</button>
-            <button onClick={substractToCounter}>-</button>
-            <button onClick={resetCounter}>reset</button>
+            <div>{ counter1 }</div>
+            <div>{ counter2 }</div>
+            <div>{ counter3 }</div>
+            <hr />
+            <button className='btn' onClick={ addToCounter }>+</button>
+            <button className='btn' onClick={ substractToCounter }>-</button>
+            <button className='btn' onClick={ resetCounter }>reset</button>
         </>
 
     )
